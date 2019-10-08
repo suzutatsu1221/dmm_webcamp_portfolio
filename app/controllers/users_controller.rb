d0f46730d027bc87f5d6890e38b7f1c35ca86353
suzutatsu <suzuta1221@gmail.com>
2019-10-08 04:13:53 +0000

    devise回り機能更新

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+
+	def top
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+  	:registrations => 'users/registrations',
+  	:sessions => 'users/sessions',
+  	:passwords => 'users/passwords'
+  }
+
+  devise_scope :user do
+
+  root 'users#top'
+  end
 end
+
