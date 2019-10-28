3c39774613281d76d87881cb9fba61403b3af65b
suzutatsu <suzuta1221@gmail.com>
2019-10-28 05:04:46 +0000

    favoritesテーブル削除

---
+++ db/migrate/20191028050248_delate_record.rb
@@ -0,0 +1,5 @@
+class DelateRecord < ActiveRecord::Migration[5.2]
+  def change
+  	drop_table :favorites
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_094358) do
-
-  create_table "favorites", force: :cascade do |t|
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "video_id"
-  end
+ActiveRecord::Schema.define(version: 2019_10_28_050248) do
 
   create_table "searches", force: :cascade do |t|
     t.datetime "created_at", null: false
