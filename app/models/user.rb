4bcb49c590531e7901a3b37c3ac9d53a9c5bfd4c
suzutatsu <suzuta1221@gmail.com>
2019-11-02 05:07:06 +0000

    userモデルのvalidation追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
     enum sex: { male: 0, female: 1, others: 2 }
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+    validates :email, presence: true
+    validates :sex, presence: true
+    validates :password, presence: true
 end
